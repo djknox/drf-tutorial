a0a82aa671858bfb3e3cb851b0f4df9886d1412d
DJ Knox <dhknox@gmail.com>
2019-08-25 16:23:00 -0400

    update SnippetSerializer and SnippetList view to associate a user with a snippet they create

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -3,8 +3,18 @@ from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
 
     class Meta:
         model = Snippet
         fields = ('id', 'title', 'code', 'linenos',
-                  'language', 'style', )
+                  'language', 'style', 'owner',)
+
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
